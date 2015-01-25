a1d08d82533d21c737855536cefcb0436dcc0692
Vasilis-Thanos Anagnostopoulos <anagnwstopoulos@hotmail.com>
2015-01-25 16:22:56 +0200

    Adding some more diagrams from matlab

--- matlab/features.m
+++ matlab/features.m
@@ -33,15 +33,72 @@ ylabel('x2');
 grid on
 hold off
 
-% Create the hierarchical clustering dendrogram.
+% Create the hierarchical clustering dendrogram with euclidean distange
+% and average linkage.
 Y = pdist(X,'euclidean');
 Z = linkage(Y,'average');
-figure('Name','Hierarchical Dendrogram')
+figure('Name','Hierarchical Dendrogram (average,euclidean)')
 [H,T] = dendrogram(Z,0,'colorthreshold','default');
 set(H,'LineWidth',2)
 
 % Cluster data in three clusters.
-T = clusterdata(X,'linkage','average',3);
-figure('Name','Identified Clusters')
+T = clusterdata(X,'linkage','average','distance','euclidean',3);
+figure('Name','Identified Clusters (average,euclidean)')
+scatter(X(:,1),X(:,2),20,T,'filled')
+grid on
+
+% Create the hierarchical clustering dendrogram with mahalanobis distange
+% and average linkage.
+Y = pdist(X,'mahalanobis');
+Z = linkage(Y,'average');
+figure('Name','Hierarchical Dendrogram (average,mahalanobis)')
+[H,T] = dendrogram(Z,0,'colorthreshold','default');
+set(H,'LineWidth',2)
+
+% Cluster data in three clusters.
+T = clusterdata(X,'linkage','average','distance','mahalanobis',3);
+figure('Name','Identified Clusters (average,mahalanobis)')
+scatter(X(:,1),X(:,2),20,T,'filled')
+grid on
+
+% Create the hierarchical clustering dendrogram with cityblock distange
+% and average linkage.
+Y = pdist(X,'cityblock');
+Z = linkage(Y,'average');
+figure('Name','Hierarchical Dendrogram (average,cityblock)')
+[H,T] = dendrogram(Z,0,'colorthreshold','default');
+set(H,'LineWidth',2)
+
+% Cluster data in three clusters.
+T = clusterdata(X,'linkage','average','distance','cityblock',3);
+figure('Name','Identified Clusters (average,cityblock)')
+scatter(X(:,1),X(:,2),20,T,'filled')
+grid on
+
+% Create the hierarchical clustering dendrogram with euclidean distange
+% and single linkage.
+Y = pdist(X,'euclidean');
+Z = linkage(Y,'single');
+figure('Name','Hierarchical Dendrogram (single,euclidean)')
+[H,T] = dendrogram(Z,0,'colorthreshold','default');
+set(H,'LineWidth',2)
+
+% Cluster data in three clusters.
+T = clusterdata(X,'linkage','single','distance','euclidean',3);
+figure('Name','Identified Clusters (single,euclidean)')
+scatter(X(:,1),X(:,2),20,T,'filled')
+grid on
+
+% Create the hierarchical clustering dendrogram with euclidean distange
+% and complete linkage.
+Y = pdist(X,'euclidean');
+Z = linkage(Y,'complete');
+figure('Name','Hierarchical Dendrogram (complete,euclidean)')
+[H,T] = dendrogram(Z,0,'colorthreshold','default');
+set(H,'LineWidth',2)
+
+% Cluster data in three clusters.
+T = clusterdata(X,'linkage','complete','distance','euclidean',3);
+figure('Name','Identified Clusters (complete,euclidean)')
 scatter(X(:,1),X(:,2),20,T,'filled')
 grid on
